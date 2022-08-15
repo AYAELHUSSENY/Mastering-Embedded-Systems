a9facf7de5ad8072da149a56848e8562e3718f3e
AYAELHUSSENY <67708664+AYAELHUSSENY@users.noreply.github.com>
2022-08-15 20:44:56 +0200

    Update EX_1.c

--- C_Programming/Unit_2/Lecture_3/Homework_2/EX_1.c
+++ C_Programming/Unit_2/Lecture_3/Homework_2/EX_1.c
@@ -1,1 +1,30 @@
+/*
+ * main.c
+
+ *
+ *  Created on: Aug 15, 2022
+ *      Author: Aya Elhusseny
+ */
+#include<stdio.h>
+ int main()
+{
+	int InputNumber;
+	printf("Enter an integer you want to check:");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&InputNumber);
+	if(InputNumber%2 == 0)
+	{
+		printf("%d is even",InputNumber);
+
+	}
+	else
+	{
+		printf("%d is odd",InputNumber);
+
+	}
+
+}
+
+
 
